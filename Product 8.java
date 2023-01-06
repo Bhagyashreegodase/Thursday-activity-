930b1821a1101aef0868379ded01c37df98a39a5
Bhagyashreegodase <118460084+Bhagyashreegodase@users.noreply.github.com>
2023-01-06 19:15:49 +0530

    Add files via upload

---
+++ Product 8.java
@@ -0,0 +1,19 @@
+
+package com.Question1;
+
+public class Product {
+
+	int id;
+	String name;
+	double price;
+	Product(int id,String name,double price){  // creating constructor
+		this.id=id;  
+		this.name=name;  
+		this.price=price; 
+	
+	
+	
+
+	}
+
+}
